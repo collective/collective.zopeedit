6baeaf2fd421207be580488d391f740b8757cffe
Godefroid Chapelle <gotcha@bubblenet.be>
2018-04-20 11:42:08 +0200

    fix tests

--- tests/test_with_server.py
+++ tests/test_with_server.py
@@ -1,10 +1,15 @@
 import os.path
 import os
 
+from sys import platform
+
 def test_instance():
     path = list()
     path.append(os.getcwd())
     path.append("tests")
     path.append("bin")
-    path.append("instance")
+    filename = "instance"
+    if platform == "win32":
+        filename += ".exe"
+    path.append(filename)
     assert os.path.exists(os.sep.join(path))
